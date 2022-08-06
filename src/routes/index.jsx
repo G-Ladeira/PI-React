4e9f9a9206ca85be9316ee8bf60cd4ee49a53db1
Lucas Figueiredo <103148467+L0uk9@users.noreply.github.com>
2022-08-06 13:59:30 -0300

    subindo página products e a nova branch (products)

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,16 @@
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Home } from "../views/home";
 import { AboutUs } from "../views/aboutUs";
+import { Products } from "../views/products";
 
-export const MainRoutes = ()=>{
-    return (
-        <>
-           <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/aboutUs" element={<AboutUs/>}/>
-                
-            </Routes> 
-        </>
-    )
-}
+export const MainRoutes = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </>
+  );
+};
--- src/views/aboutUs/index.jsx
+++ src/views/aboutUs/index.jsx
@@ -1,38 +1,34 @@
-import '../../styles/global/main.sass'
-
+import "../../styles/global/main.sass";
 
 export const AboutUs = () => {
-    return (
-        <>
-        <div>
+  return (
+    <>
+      <div>
         <main>
-            <div className="bandeira">
-                <div>
-                </div>
+          <div className="bandeira">
+            <div></div>
+          </div>
+          <div className="objetivos">
+            <div className="titulo">
+              <h1>Objetivos da marca</h1>
             </div>
-            <div className="objetivos">
-                <div className="titulo">
-                    <h1>Objetivos da marca</h1>
-                </div>
-                <div className="joias6">
-                </div>
-                <div className="sobre">
-                    <p>
-                        Trazer ao mercado um produto de design diferenciado e alegre,
-                        desenvolvido no Brasil. A marca leva o DNA da família de Wendy,
-                        que se inspirou principalmente no trabalho de esmaltação na
-                        joalheria, muito característica da joalheria inglesa.
-                        Temos a missão de levar o “Joie de Vivre” através do nosso design
-                        alegre, atemporal e sem gêneros além de elevar a auto-estima
-                        de quem as usa e criar uma forte ponte emocional com público
-                        brasileiro e internacional fazendo-os sentir-se feliz e muito
-                        especial através do nosso design vintage afetivo.
-                    </p>
-                </div>
+            <div className="joias6"></div>
+            <div className="sobre">
+              <p>
+                Trazer ao mercado um produto de design diferenciado e alegre,
+                desenvolvido no Brasil. A marca leva o DNA da família de Wendy,
+                que se inspirou principalmente no trabalho de esmaltação na
+                joalheria, muito característica da joalheria inglesa. Temos a
+                missão de levar o “Joie de Vivre” através do nosso design
+                alegre, atemporal e sem gêneros além de elevar a auto-estima de
+                quem as usa e criar uma forte ponte emocional com público
+                brasileiro e internacional fazendo-os sentir-se feliz e muito
+                especial através do nosso design vintage afetivo.
+              </p>
             </div>
+          </div>
         </main>
-        </div>
-        </>
-    )
-
-}
+      </div>
+    </>
+  );
+};
