c73f2c3ae41adbbd7613dd0cf93367bff3c1ddfd
Gabriel Ladeira Gomes Soares <105513969+G-Ladeira@users.noreply.github.com>
2022-08-09 17:28:47 -0300

    criação do header e criação da home (#2)
    Co-authored-by: Veloso-dev <velosoarts@gmail.com>

---
+++ src/views/home/Items/Itemss.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import img from './img/img.png'
+
+import './items.sass'
+
+
+
+
+
+
+const Itemss = () => {
+    return (
+
+        <section>
+
+            <div>
+
+                <img src={img} alt="img" ></img>
+
+            </div>
+
+            <div>
+
+                <img src={img} alt="img" ></img>
+
+            </div>
+
+            <div>
+
+                <img src={img} alt="img" ></img>
+
+            </div>
+
+            <div>
+
+                <img src={img} alt="img" ></img>
+
+            </div>
+
+
+
+        </section>
+    )
+}
+
+export default Itemss
