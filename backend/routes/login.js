707d88e182c25180b8877dd5ad6a9827873b8b0d
Gabriel Ladeira Gomes Soares <ladeiragabriel12@gmail.com>
2022-09-02 21:43:36 -0300

    subindo backend

---
+++ backend/routes/login.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+let dataReturn = {
+    "status": "error",
+    "menssage": "Requisição Inválida",
+    "data": null
+};
+function returnSuccess(menssage,data = null){
+    dataReturn.status = 'success'
+    dataReturn.menssage = menssage
+    dataReturn.data = data
+    return dataReturn
+}
+
+function returnError(menssage,data = null){
+    dataReturn.status = 'Error'
+    dataReturn.menssage = menssage
+    dataReturn.data = data
+    return dataReturn
+}
+
+
+
+    router.post('/', function (req, res, next) {
+        const nome = req.body.nome
+        const pwd = req.body.password
+
+        if (nome == 'Kinder' && pwd == 'paupequeno') {
+            let dadosUsuario = "xablau"
+            res.json(returnSuccess('Usuário Logado com Sucesso!', dadosUsuario))
+        } else {
+            res.json(returnError('Falha ao logar'))
+        }
+
+    });
+
+module.exports = router;
