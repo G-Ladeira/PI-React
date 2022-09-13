3087127bdfa5bb02e7028724f1e08d6374697326
Gabriel Ladeira Gomes Soares <ladeiragabriel12@gmail.com>
2022-09-13 19:00:39 -0300

    Dinamizando info product

--- src/components/product/info_section/index.jsx
+++ src/components/product/info_section/index.jsx
@@ -1,42 +1,63 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 import './style.sass'
+
+const url = "https://mocki.io/v1/9234e80f-9572-4d0a-8d55-e7020e912a3a"
+
+
 
 
 
 const Info = () => {
-  return (
-    <div className="product-info">
-                    <span className="prdt-colection">
-                        Coleção do produto
-                    </span>
 
-                    <h1 className="product-name">Nome do produto</h1>
+    const [repositories, setRepositories] = useState("")
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => setRepositories(data))
 
-                    <h2 className="product-price">R$99.999,99</h2>
+        }, 2000);
+    }, []);
 
-                    <form>
-                        <div className="size">
-                            <select name='tamanhos'>
-                                <option value="" defaultChecked>Tamanho</option>
-                                <option value="10">10</option>
-                                <option value="20">20</option>
-                                <option value="25">25</option>
-                            </select>
-                        </div>
+    // try {
 
-                        <div className="quantity">
-                            <select name='quantidade'>
-                                <option value="" defaultChecked>Quantidade</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                            </select>
-                        </div>
+    let cardProduto = <>
+        <span className="prdt-colection">Coleção do produto</span>
 
-                        <button className='btnAdd'>Adcionar à sacola</button>
-                    </form>
-                </div>
-  )
+        <h1 className="product-name">{repositories.nome}</h1>
+
+        <h2 className="product-price">R$99.999,99</h2>
+
+        <form>
+            <div className="size">
+                <select name='tamanhos'>
+                    <option value="" defaultChecked>Tamanho</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="25">25</option>
+                </select>
+            </div>
+
+            <div className="quantity">
+                <select name='quantidade'>
+                    <option value="" defaultChecked>Quantidade</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>
+            </div>
+
+            <button className='btnAdd'>Adcionar à sacola</button>
+        </form>
+    </>
+
+    let carregando = <h1 className="product-name">Carregando ...</h1>
+    return (
+        <div className="product-info">
+            {repositories ? cardProduto : carregando}
+        </div>
+    )
 }
+
 
 export default Info
