484cdc68dfef3668af8abc414c5dbfb3bafe474b
Felipe Paiva <104072028+FeKinDeR@users.noreply.github.com>
2022-09-25 14:59:06 -0300

    arrumandoheader

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./sytle.sass"
 import { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
+
 
 const url = "https://mocki.io/v1/c12e210f-6d15-46ab-b5ae-9db8b138ea2b"
 
@@ -12,82 +14,56 @@ const ProductsC = () => {
       fetch(url)
         .then(response => response.json())
         .then(data => setRepositories(data))
-    
     }, []);
-    
-    
       return (
-    
         <div>
         <h2 className='produto' >X produtos encontrados</h2>
-    
         <section className='sectionGrid1'>
-            
-            
-    
     {repositories.map((p, i) => {
-    
     return <article >
       <div >
+        <Link to={"/product"}>
         <img
           className="img_products1"
           src="../images/joias2.png"
           alt="product"
         />
+        </Link>
       </div>
-      <div>
+      <div className='infoP'>
         <p>{p.nome}</p>
         <p>{p.descricao}</p>
       </div>
-
-
       <div  className='linha2'>
+      <Link to={"/product"}>
         <img
           className="img_products1"
           src="../images/joias2.png"
           alt="product"
         />
+       </Link>
       </div>
-      <div>
+      <div className='infoP'>
         <p>{p.nome}</p>
         <p>{p.descricao}</p>
       </div>
-
       <div  className='linha3'>
+        <Link to={"/product"}>
         <img
           className="img_products1"
           src="../images/joias2.png"
           alt="product"
         />
+        </Link>
       </div>
-      <div>
+      <div className='infoP'>
         <p>{p.nome}</p>
         <p>{p.descricao}</p>
       </div>
-
-      
-
     </article>
-    
-    
     })}
-    
-    
-    
-    
           </section>
-    
           </div>
       )}
-    
     export default ProductsC
-
-
-
-
-
-
-
-
-
 
