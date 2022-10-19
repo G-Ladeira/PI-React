e579e014c475981e8be53a469338fac2da499df5
Felipe Paiva <104072028+FeKinDeR@users.noreply.github.com>
2022-10-19 18:21:44 -0300

    arrumando cadastro

--- src/components/cadastroUsuario/index.jsx
+++ src/components/cadastroUsuario/index.jsx
@@ -1,79 +1,49 @@
-import React from 'react'
+import { SettingsSuggestRounded } from '@mui/icons-material'
+import React,{useState} from 'react'
 import "./cadastro.sass"
 
-const UserForm = props => {
-  const [user, setUser] = useState(props.user)
-
-  const submit = e => {
-    e.preventDefault()
-    fetch('/cadastro', {
-      method: 'POST',
-      body: JSON.stringify({ user }),
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then(res => res.json())
-      .then(json => setUser(json.user))
-  }
-
-  return (
-    <form onSubmit={submit}>
-      <input
-        type="text"
-        name="user[name]"
-        value={user.name}
-        onChange={e => setUser({ ...user, name: e.target.value })}
-      />
-      {user.errors.name && <p>{user.errors.name}</p>}
-      <input
-        type="password"
-        name="password[password]"
-        value={user.name}
-        onChange={e => setUser({ ...user, password: e.target.value })}
-      />
-      {user.errors.name && <p>{user.errors.name}</p>}
-
-      <input
-        type="email"
-        name="user[email]"
-        value={user.email}
-        onChange={e => setUser({ ...user, email: e.target.value })}
-      />
-      {user.errors.name && <p>{user.errors.name}</p>}
-
-      <input type="submit" name="Sign Up" />
-    </form>
-  )
-}
-
-
-////////////////////////////////////////////////////////////////
 const CadastroUsuario = () => {
+    const [nome, setNome] = useState("")
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+    const [confirmarSenha, setConfirmarSenha] = useState("")
+    const [erro, setErro] = useState("")
+    const [sucesso, setSucesso] = useState("")
+    const [loading, setLoading] = useState(false)
+    
   return (
-    <main className='cadastroC'>
-    <div id='continer2'>
-
-      <div className='boxLogin'>
-          <h1 id='textCriar'>Criar Seu Cadastro</h1>
-
-          <form action='/cadastro' method='POST' className='boxAccount'>
-
-            <input type="text" name='Nome' id='nomeCad' placeholder='Nome'/>
-            <input type="email" name='email' id='email' placeholder='E-mail'/>
-            <input type="email" name='email' id='email' placeholder='Confirmar E-mail'/>
-            <input type="password" name='password' id='passwordCad' placeholder='Senha'/>
-            <input type="password" name='password' id='passwordCad' placeholder='Confirmar Senha'/>
-
-            <button type='submit' id='buttonCriar'> Criar Conta</button>
+    
+    <div className="cadastro">
+      <div className="cadastro__container">
+        <div className="cadastro__container__titulo">
+          <h1>Cadastro</h1>
+        </div>
+        <div className="cadastro__container__form">
+          <form>
+            <div className="cadastro__container__form__nome">
+              <label>Nome</label>
+              <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} />
+            </div>
+            <div className="cadastro__container__form__email">
+              <label>Email</label>
+              <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <div className="cadastro__container__form__senha">
+              <label>Senha</label>
+              <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)} />
+            </div>
+            <div className="cadastro__container__form__confirmarSenha">
+              <label>Confirmar Senha</label>
+              <input type="password" value={confirmarSenha} onChange={(e) => setConfirmarSenha(e.target.value)} />
+            </div>
+            <div className="cadastro__container__form__botao">
+              <button type="submit">Cadastrar</button>
+            </div>
           </form>
-
+        </div>
       </div>
-
     </div>
 
-
-    </main>
-
   )
 }
-
 export default CadastroUsuario
