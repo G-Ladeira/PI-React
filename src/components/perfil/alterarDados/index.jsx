8ecf21dc83768d04d50cf8f03b20129a8df5a141
Gabriel Ladeira Gomes Soares <ladeiragabriel12@gmail.com>
2022-10-26 17:55:58 -0300

    ULTIMO COMMIT! Finalização de perfil

--- src/components/perfil/alterarDados/index.jsx
+++ src/components/perfil/alterarDados/index.jsx
@@ -1,39 +1,52 @@
-import React from 'react'
-
-
+import React, { useState, useEffect } from 'react'
+import api from '../../../services';
 import "./style.sass";
 
+
+
 const AlterCadastro = (props) => {
-  
+
+  const [nome, setNome] = useState()
+  const [email, setEmail] = useState()
+  const [telefone, setTelefone] = useState()
+  const user = localStorage.getItem("WT::token");
+  console.log(user)
+  const aaaa = function (){
+      api.patch('users/alterar',{nome, email, telefone, token:user})
+  }
+  const bbbb = async ()=>{
+    const user = localStorage.getItem("WT::token");
+    const result = await api.post("/users/perfil",{token:user})
+    console.log(result.data)
+    setNome(result.data.nome)
+    setEmail(result.data.email)
+    setTelefone(result.data.telefone)
+}
+useEffect(()=>{
+    bbbb()
+},[])
+
   return (
 
     < div className = "userDataSection" >
-
-
-
-
-
 
 
     <div className='userDataBox'>
       <h1 id='dataTitle'>Alterar dados</h1>
 
       <div className='dataForm'>
-        <label htmlFor='Nome' className='labelForm'>Nome Completo</label>
-        <input type="text" name='Nome' id='altCad' placeholder='Nome' />
+        <label  htmlFor='Nome' className='labelForm'>Nome Completo</label>
+        <input onChange={(value)=> setNome(value.target.value)} value= {nome} type="text" name='Nome' id='altCad' placeholder='Nome' />
 
         <label htmlFor='email' className='labelForm'>E-mail</label>
-        <input type="email" name='email' id='altEmail' placeholder='E-mail' />
+        <input onChange={(value)=> setEmail(value.target.value)} value= {email}  type="email" name='email' id='altEmail' placeholder='E-mail' />
 
         <label htmlFor='telefone' className='labelForm'>Telefone</label>
-        <input type="tel" name="telefone" id='altTel' placeholder='Telefone' />
-
-        <label htmlFor='Senha' className='labelForm'>Senha</label>
-        <input type="text" name="Senha" id='altSenha' placeholder='Senha' />
+        <input onChange={(value)=> setTelefone(value.target.value)} value= {telefone}  type="tel" name="telefone" id='altTel' placeholder='Telefone' />
 
 
 
-        <button onClick={()=> props.setActive('atualizar')} className='editdata2'>Salvar</button>
+        <button onClick={aaaa} className='editdata2'>Salvar</button>
 
 
       </div>
