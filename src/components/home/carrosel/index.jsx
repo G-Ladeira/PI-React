cd0384c679d7ce5ae4510d010b6934ff167fccf3
Veloso-dev <74386171+Veloso-dev@users.noreply.github.com>
2022-10-26 11:58:58 -0300

    alteração carrousel

--- src/components/home/carrosel/index.jsx
+++ src/components/home/carrosel/index.jsx
@@ -15,11 +15,9 @@ const CarouselHome = () => {
       <img src="../images/arte-carrousel.jpg" className="d-block w-100" alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src="../images/arte-carrousel.jpg" className="d-block w-100" alt="..."/>
+      <img src="../images/arte.jpg" className="d-block w-100" alt="..."/>
     </div>
-    <div className="carousel-item">
-      <img src="../images/arte-carrousel.jpg" className="d-block w-100" alt="..."/>
-    </div>
+    
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
--- src/components/perfil/alterarDados/index.jsx
+++ src/components/perfil/alterarDados/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 import "./style.sass";
 
-const AlterCadastro = () => {
+const AlterCadastro = (props) => {
   
   return (
 
